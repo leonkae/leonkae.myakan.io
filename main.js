2eb7801016735b14f12c4a090c947c74de8ce386
leonkae <leon29kae@gmail.com>
2022-03-04 00:03:18 +0300

    Building index

--- main.js
+++ main.js
@@ -2,9 +2,15 @@
 const yearSelect = document.getElementById("year");
 const monthSelect = document.getElementById("month");
 const daySelect = document.getElementById("day");
-const daySelect = document.getElementById("gender-selector");
-const daySelect = document.getElementById("gender-selector");
+const genderSelect = document.getElementById("gender-selector");
+const birthdaySelect = document.getElementById("birthday-selector");
+const genderIndex = document.querySelector(".gender").selectedIndex
+const gender = document.querySelector(".gender").options;
 
 
 
+console.log (gender[genderIndex])
 
+function ValidateForm(event){
+    event.preventDefault()
+}
